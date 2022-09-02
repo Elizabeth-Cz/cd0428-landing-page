66c2bae1b6d389765fac422b0e116c81a5844b41
Elizabeth Czarny <liz.cz91@gmail.com>
2022-09-02 15:32:42 +0200

    added form

--- js/app.js
+++ js/app.js
@@ -1,73 +1,79 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
-const navEl = document.querySelector(".navbar__menu")
-const navListEl = document.querySelector("#navbar__list")
-const sections = document.querySelectorAll("section")
-
-console.log(sections);
-
-sections.forEach((section)=>{
-    const navItem = document.createElement("li");
-    navItem.textContent = section.querySelector("h2").textContent;
-    navListEl.appendChild(navItem);
-})
+const navEl = document.querySelector(".navbar__menu");
+const navListEl = document.querySelector("#navbar__list");
+const sections = document.querySelectorAll("[data-nav]");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
+let navListAnchor;
 
+sections.forEach((section) => {
+  const navListItem = document.createElement("li");
+  navListAnchor = document.createElement("a");
+  navListAnchor.classList.add("menu__link");
+  navListAnchor.textContent = section.dataset.nav;
+  navListAnchor.href = `#${section.id}`;
+  navListItem.append(navListAnchor);
+  navListEl.appendChild(navListItem);
+  console.log(section);
+});
+
+// try for of/for in loop for navigation
+
+// get attribute data-nav
+// sections.dataSet.nav
 
 // Add class 'active' to section when near top of viewport
-
-
+function setActive() {
+  for (const item of navListEl) {
+    console.log(item);
+  }
+}
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
