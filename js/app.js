d3d2da2a0fc57d704e17373eec17c396f01b143a
Elizabeth Czarny <liz.cz91@gmail.com>
2022-09-02 17:38:47 +0200

    added smooth scrolling

--- js/app.js
+++ js/app.js
@@ -50,30 +50,46 @@ sections.forEach((section) => {
   navListAnchor.href = `#${section.id}`;
   navListItem.append(navListAnchor);
   navListEl.appendChild(navListItem);
-  console.log(section);
+  // console.log(navListEl);
 });
+
+for (let i = 0; i < navListEl.length; i++) {
+  console.log(navListEl[i]);
+}
 
 // try for of/for in loop for navigation
 
-// get attribute data-nav
-// sections.dataSet.nav
-
 // Add class 'active' to section when near top of viewport
-function setActive() {
-  for (const item of navListEl) {
-    console.log(item);
+function makeActive() {
+  for (const section of sections) {
+    const box = section.getBoundingClientRect();
+    //Find a value that works best, but 150 seems to be a good start.
+    if (box.top <= 150 && box.bottom >= 150) {
+      // console.log(section);
+      section.classList.add("selected");
+      // console.log(document.querySelector(`[href="#${section.id}"]`));
+      document.querySelector(`[href="#${section.id}"]`).classList.add("active");
+      console.log();
+      //apply active state on current section and corresponding Nav link
+    } else {
+      section.classList.remove("selected");
+      document
+        .querySelector(`[href="#${section.id}"]`)
+        .classList.remove("active");
+      //Remove active state from other section and corresponding Nav link
+    }
   }
 }
-// Scroll to anchor ID using scrollTO event
+document.addEventListener("scroll", function () {
+  makeActive();
+});
 
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+const anchorList = document.querySelectorAll("a");
+for (const anchor of anchorList) {
+  anchor.addEventListener("click", (e) => {
+    e.preventDefault();
+    document
+      .querySelector(e.target.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+}
