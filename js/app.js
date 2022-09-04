c5a4cbaac475f11e8f6f7ef8e8ca0de294066bfc
Elizabeth Czarny <liz.cz91@gmail.com>
2022-09-04 04:53:22 +0200

    fixed console error and added helper function

--- js/app.js
+++ js/app.js
@@ -30,18 +30,19 @@ const sections = document.querySelectorAll("[data-nav]");
 // build the nav
 let navListAnchor;
 
-sections.forEach((section) => {
-  // create list item for each section
+// function to create navbar item
+const createItem = (section) => {
   const navListItem = document.createElement("li");
-  // create anchor to append to list item
   navListAnchor = document.createElement("a");
   navListAnchor.classList.add("menu__link");
-  // set the content of the anchor to the data-nav attribute
   navListAnchor.textContent = section.dataset.nav;
-  //set the href of the anchor to the id of each section
   navListAnchor.href = `#${section.id}`;
   navListItem.append(navListAnchor);
   navListEl.appendChild(navListItem);
+};
+
+sections.forEach((section) => {
+  createItem(section);
 });
 
 for (let i = 0; i < navListEl.length; i++) {
@@ -52,26 +53,23 @@ for (let i = 0; i < navListEl.length; i++) {
 function makeActive() {
   for (const section of sections) {
     const box = section.getBoundingClientRect();
-    //Find a value that works best, but 150 seems to be a good start.
+    //apply active state on current section and corresponding Nav link
     if (box.top <= 150 && box.bottom >= 150) {
-      // console.log(section);
       section.classList.add("selected");
-      // console.log(document.querySelector(`[href="#${section.id}"]`));
       document.querySelector(`[href="#${section.id}"]`).classList.add("active");
-      console.log();
-      //apply active state on current section and corresponding Nav link
     } else {
+      //Remove active state from other section and corresponding Nav link
       section.classList.remove("selected");
       document
         .querySelector(`[href="#${section.id}"]`)
         .classList.remove("active");
-      //Remove active state from other section and corresponding Nav link
     }
   }
 }
 document.addEventListener("scroll", function () {
   makeActive();
 });
+
 // get a node list of all anchors
 const anchorList = document.querySelectorAll("a");
 // add an event listener for each anchor for smooth scrolling
